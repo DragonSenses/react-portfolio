4086f2315f1be7439eea71fc5895720469a92e2b
DragonSenses <kendrickfrancisco@gmail.com>
2023-02-23 14:56:16 -0800

    Update Aboutme component

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import styles from './aboutme.module.css'
 
 export default function Aboutme() {
   return (
-    <section id='about-me'>
-      About me
+    <section id='about-me' className={styles.aboutMe}>
+      <div className={styles.wrapper}>
+        <div className={styles.headerText}>
+          <div className={styles.line}></div>
+          <h1 className={styles.text}>About Me</h1>
+          <div className={styles.line}></div>
+        </div>
+      </div>
     </section>
   )
 }
