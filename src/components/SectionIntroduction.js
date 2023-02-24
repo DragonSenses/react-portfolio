90dc2c4dc3a16e0b181674b9a6db791bbe664aac
DragonSenses <kendrickfrancisco@gmail.com>
2023-02-24 15:07:03 -0800

    Update SectionIntroduction.js

--- src/components/SectionIntroduction.js
+++ src/components/SectionIntroduction.js
@@ -2,12 +2,12 @@ import React from 'react'
 import styles from './sectionintroduction.module.css'
 
 export default function SectionIntroduction(props) {
-  const { children } = props;
+  const { children, dark } = props;
   return (
     <div className={styles.headerText}>
-      <div className={styles.line}></div>
-      <h1 className={styles.text}>{children}</h1>
-      <div className={styles.line}></div>
+    <div className={(dark ? styles.lineDark: styles.line)}></div>
+      <h1 className={(dark ? styles.textDark: styles.text)}>{children}</h1>
+      <div className={(dark ? styles.lineDark: styles.line)}></div>
     </div>
   )
 }
