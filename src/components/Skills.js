40762af0366bec7f52f7769232059b2a9f382542
DragonSenses <kendrickfrancisco@gmail.com>
2023-02-27 15:28:27 -0800

    Create Skills Component

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import styles from './skills.module.css'
+import SectionIntroduction from './SectionIntroduction'
 
 export default function Skills() {
   return (
     <section id='skills'>
-      Skills
+      <SectionIntroduction dark={true}>SKILLS</SectionIntroduction>
+
+      <div className={styles.sectionContent}>
+
+      </div>
     </section>
   )
 }
